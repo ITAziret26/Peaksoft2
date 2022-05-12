a0c55e5e2556e92ae2791717725713f04315f05f
ITAziret26 <azirettoichubekov263@gmail.com>
2022-05-12 14:37:00 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// 1
+// let num = "" + 1 + 0;
+// console.log(num)
+
+// 2
+// let num ="" - 1 + 0;
+// console.log(num)
+
+// 3
+// let num = 1;
+// let num1 = 0;
+// console.log(Boolean(num1));
+// console.log(Boolean(num));
+
+// 4
+// let num = 6/"3"
+// console.log(num)
+
+// 5
+// let num = "2" * "3"
+// console.log(num)
+// 6
+// let num = 4 + 5 +"px"
+// console.log(num)
+// 7
+// let num ="$" + 4 +5
+// console.log(num)
+
+// 8
+// let num ="4" - 2
+// console.log(num)
+
+// 9
+// let num ="4px" -2;
+// console.log(num)
+
+// 10
+// let num = 7/0
+// console.log(num)
+
+// 11
+// let num = "  -9" +5
+// console.log(num)
+
+// 12
+// let num = "  -9" -5
+// console.log(num)
+
+// 13
+// let num = null +1;
+// console.log(num)
+
+// 14
+// let num =undefined +1;
+// console.log(num)
+
+// 15
+// let num ="\t\n" - 2
+// console.log(num)
+
+// Что выведет этот скрип?
+// 1
+// let name ="Ilya"
+// console.log(name);Ilya
+
+// 1
+// alert(`hello ${'1'}`);hello 1
+// 2
+// alert(`hello ${'name'}`);hello name
